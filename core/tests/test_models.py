6b75107c39165b5b56d1ff08ed96465c552c5e18
Victor Fedorov <vpfedorov25@gmail.com>
2026-03-16 23:13:05 +0800

    empty state for review

--- core/tests/test_models.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from core.models import BalanceLedger
-
-class BalanceModelTests(TestCase):
-    def test_happy_path(self):
-        b = BalanceLedger.objects.create(balance=100)
-        self.assertEqual(str(b), "100")
