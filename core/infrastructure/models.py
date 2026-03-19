a547c41fd3e6b9d8f55171550f53bb4bfc029365
Victor Fedorov <vpfedorov25@gmail.com>
2026-03-19 23:44:10 +0800

    добавил миксины

--- core/infrastructure/models.py
+++ core/infrastructure/models.py
@@ -2,10 +2,26 @@ from django.db import models
 import uuid
 
 
+
+
 class Status(models.TextChoices):
         PENDING = 'PENDING', 'Ожидает оплаты'
         PAID = 'PAID', 'Оплачен'
         CANCELED = 'CANCELED', 'Отменен'
+
+class TimestampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    class Meta:
+        abstract = True
+
+
+class IdempotencyMixin(models.Model):
+    idempotency_key = models.CharField(max_length=100, unique=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+    
 
 class User(models.Model):
     name = models.CharField("Имя пользователя", max_length=200)
@@ -15,11 +31,9 @@ class User(models.Model):
         return f"{self.name}"
 
 
-class Order(models.Model):
-    idempotency_key = models.CharField(max_length=100, unique=True, default=uuid.uuid4)
+class Order(TimestampMixin, IdempotencyMixin):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
     total_price = models.IntegerField(default=0, verbose_name="Цена")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     status = models.CharField(max_length=10, choices=Status.choices, default=Status.PENDING, verbose_name="Статус")
 
     def __str__(self):
@@ -57,29 +71,22 @@ class Wallet(models.Model):
     def __str__(self):
         return f"Пользователь {self.user} имеет {self.balance} на счету"
 
-class IdempotencyRecords(models.Model):
-    idempotency_key = models.CharField("Ключ идемпотентности", max_length=200, unique=True, default=uuid.uuid4)
+class IdempotencyRecords(TimestampMixin, IdempotencyMixin):
     namespace = models.CharField(max_length=200)
     status = models.CharField(max_length=10, choices=Status.choices, default=Status.PENDING)
-    created_at = models.DateTimeField(auto_now_add=True )
 
 
-
-class Refund(models.Model):
-    idempotency_key = models.CharField("Ключ идемпотентности", max_length=200, unique=True, default=uuid.uuid4)
+class Refund(TimestampMixin, IdempotencyMixin):
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
     reason = models.CharField(max_length=200)
-    created_at =models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Заказ {self.order} был вовращен {self.created_at} по причине: {self.reason}"
 
 
-class Payment(models.Model):
-    idempotency_key = models.CharField("Ключ идемпотентности", max_length=200, unique=True, default=uuid.uuid4)
+class Payment(TimestampMixin, IdempotencyMixin):
     wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, verbose_name="Кошелек пользователя")
     order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name="Заказ")
-    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
     status = models.CharField(max_length=10, choices=Status.choices, default=Status.PENDING, verbose_name="Статус платежа")
     price = models.IntegerField("Сумма платежа")
 
