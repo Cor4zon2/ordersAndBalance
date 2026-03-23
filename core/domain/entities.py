43618108495b4911b85748bbacf9d1b3fec0186f
Victor Fedorov <vpfedorov25@gmail.com>
2026-03-23 21:45:45 +0800

    refactor: убрал мок-функции

--- core/domain/entities.py
+++ core/domain/entities.py
@@ -3,34 +3,6 @@ from typing import List
 from enum import Enum
 
 
-def create_order(order):
-    return {
-        "id": "mock-id",
-        "items": [
-            {"id": 1, "name": "Iphone 17", "amount": 3},
-            {"id": 2, "name": "New TV", "amount": 1},
-            {"id": 3, "name": "Book", "amount": 2}
-            ],
-        "price": 1000,
-        "clientId": 101,
-        "status": "PAID"
-    }
-
-
-def get_order_by_id(id):
-    return {
-        "id": "mock-id",
-        "items": [
-            {"id": 21, "name": "Socks", "amount": 1},
-            {"id": 32, "name": "MusicBox", "amount": 1},
-            {"id": 13, "name": "Book", "amount": 22}
-            ],
-        "price": 55,
-        "clientId": 10,
-        "status": "PENDING"
-    }
-
-
 class InsufficientFundsError(Exception):
     pass
 
